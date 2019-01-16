0dbca145cdaa762b06713610003b13d1cab433d0
kmr1188 <kmr1188@rit.edu>
2019-01-16 12:36:55 -0500

    Added some registration exception handling

--- Workflow/Workflow/Firebase.cs
+++ Workflow/Workflow/Firebase.cs
@@ -17,27 +17,35 @@ namespace Workflow
         public static bool CreateNewUser(String email, String pass, String displayName, bool verificationEmail)
         {
             FirebaseAuthLink auth = FirebaseUtil.AttemptCreateNewUser(email, pass, displayName, verificationEmail);
-            if(auth.User != null)
+            if (auth != null)
             {
-                return true;
+                if (auth.User != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
-            else
-            {
-                return false;
-            }
+            return false;
         }
 
         public static bool LoginUser(String email, String pass)
         {
             FirebaseAuthLink auth = FirebaseUtil.AttemptLoginUser(email, pass);
-            if (auth.User != null)
+            if (auth != null)
             {
-                return true;
+                if (auth.User != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
-            else
-            {
-                return false;
-            }
+            return false;
         }
     }
 
@@ -47,16 +55,35 @@ namespace Workflow
         {
             var authProvider = Firebase.GetAuthProvider();
             Task<FirebaseAuthLink> task = authProvider.CreateUserWithEmailAndPasswordAsync(email, pass, displayName, verificationEmail);
-            task.Wait();
-            return task.Result;
+            try
+            {
+                task.Wait();
+                return task.Result;
+            }
+            catch(Exception e)
+            {
+                if(e.InnerException.Message.Contains("Reason: EmailExists"))
+                {
+                    //email exists error
+                }
+            }
+            return null;
         }
+
 
         public static FirebaseAuthLink AttemptLoginUser(String email, String pass)
         {
             var authProvider = Firebase.GetAuthProvider();
             Task<FirebaseAuthLink> task = authProvider.SignInWithEmailAndPasswordAsync(email, pass);
-            task.Wait();
-            return task.Result;
+            try
+            {
+                task.Wait();
+                return task.Result;
+            }
+            catch (Exception e)
+            {
+            }
+            return null;
         }
     }
 }
