a27c8936d6ca25ae4f305eb39e0e4136a0be09ce
bmm723 <bmm7238@rit.edu>
2019-02-03 18:22:47 -0500

    adding workflows

---
+++ Workflow/Workflow/Data/WorkflowComponentUtil.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using Workflow.Models;
+
+namespace Workflow.Data
+{
+    public class WorkflowComponentUtil
+    {
+        public static WorkflowComponent CreateWorkflowComponent(int workflowID, string componenttitle, string componenttext)
+        {
+            WorkflowComponent w = new WorkflowComponent(workflowID, componenttitle, componenttext);
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO WorkflowComponents (WorkflowID, ComponentTitle, ComponentText) VALUES (@workflowID, @componenttitle, @componenttext)");
+            cmd.Parameters.AddWithValue("@workflowID", workflowID);
+            cmd.Parameters.AddWithValue("@componenttitle", componenttitle);
+            cmd.Parameters.AddWithValue("@componenttext", componenttext);
+            DBConn conn = new DBConn();
+            conn.ExecuteInsertCommand(cmd);
+            return w;
+        }
+        public static List<WorkflowComponent> GetWorkflowComponents(int workflowID)
+        {
+            string query = "SELECT ComponentTitle, ComponentText FROM WorkflowComponents WHERE WorkflowID = (@workflowID)";
+            
+            MySqlCommand cmd = new MySqlCommand(query);
+            cmd.Parameters.AddWithValue("@workflowID", workflowID);
+            DBConn conn = new DBConn();
+            MySqlDataReader dr = conn.ExecuteSelectCommand(cmd);
+
+            List<WorkflowComponent> componentList = new List<WorkflowComponent>();
+            while (dr.Read())
+            {
+                WorkflowComponent w = new WorkflowComponent((int)dr["WorkflowID"], (string)dr["ComponentTitle"], (string)dr["ComponentText"]);
+                componentList.Add(w);
+            }
+            conn.CloseConnection();
+            return componentList;
+        }
+    }
+}
---
+++ Workflow/Workflow/Models/WorkflowComponent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Workflow.Models
+{
+    public class WorkflowComponent
+    {
+        int workflowid;
+        int wfcomponentid;
+        string componenttitle;
+        string componenttext;
+
+        public WorkflowComponent(int workflowId, string componenttitle, string componenttext)
+        {
+            this.workflowid = workflowId;
+            this.componenttext = componenttext;
+            this.componenttitle = componenttitle;
+        }
+
+        public int WorkflowId
+        {
+            get { return workflowid; }
+            set { workflowid = value; }
+        }
+
+        public string ComponentTitle
+        {
+            get { return componenttitle; }
+            set { componenttitle = value; }
+        }
+
+        public string ComponentText
+        {
+            get { return componenttext;  }
+            set { componenttext = value; }
+        }
+
+    }
+}
