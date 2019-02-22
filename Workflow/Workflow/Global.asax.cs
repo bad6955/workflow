51f2891a4af26647cc9d7079f46f7c7ec876b2c5
Shawn Bishop <sdb3018@g.rit.edu>
2019-02-21 21:37:42 -0500

    Added Log class

--- Workflow/Workflow/Global.asax.cs
+++ Workflow/Workflow/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,21 @@ namespace Workflow
 {
     public class Global : System.Web.HttpApplication
     {
+        public Global()
+        {
+            Log.Init();
+        }
+
         protected void Application_Start(object sender, EventArgs e)
         {
            
         }
+
+        public override void Dispose()
+        {
+            Log.Shutdown();
+            base.Dispose();
+        }
+
     }
 }
---
+++ Workflow/Workflow/Log.cs
@@ -0,0 +1,56 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Workflow
+{
+    public class Log
+    {
+        private static Log instance;
+        private Serilog.Core.Logger logger;
+
+        public static void Init()
+        {
+            if (instance != null)
+            {
+                return;
+            }
+            else
+            {
+                instance = new Log();
+            }
+        }
+
+        public static void Shutdown()
+        {
+            instance.logger.Dispose();
+        }
+
+        private Log()
+        {
+            logger = new LoggerConfiguration().WriteTo.File("log.txt").CreateLogger();
+        }
+
+        public static void Info(String info)
+        {
+            instance.logger.Information(info);
+        }
+        
+        public static void Warn(String warning)
+        {
+            instance.logger.Warning(warning);
+        }
+
+        public static void Debug(String debug)
+        {
+            instance.logger.Debug(debug);
+        }
+    
+        public static void Error(String error)
+        {
+            instance.logger.Error(error);
+        }
+    }
+}
