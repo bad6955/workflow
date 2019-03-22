a42b9f3f1aaa5b5a7b3453b689283c569d4dd3a2
Brie <bmm7238@rit.edu>
2019-03-22 18:20:55 -0400

    helpsystem util & data class

---
+++ Workflow/Workflow/Data/HelpSystemUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Web;
+using Workflow.Models;
+using Workflow.Utility;
+
+namespace Workflow.Data
+{
+    public class HelpSystemUtil
+    {
+        public static List<HelpSystem> GetHelp(int pageID, int roleID)
+        {
+            string query = "SELECT PageID, RoleID, Text from HelpSystem WHERE PageID=@pageID AND RoleID=@roleID";
+
+            MySqlCommand cmd = new MySqlCommand(query);
+            cmd.Parameters.AddWithValue("@pageID", pageID);
+            cmd.Parameters.AddWithValue("@roleID", roleID);
+            DBConn conn = new DBConn();
+            MySqlDataReader dr = conn.ExecuteSelectCommand(cmd);
+
+            List<HelpSystem> helpSystem = new List<HelpSystem>();
+            while (dr.Read())
+            {
+                HelpSystem hs = new HelpSystem((int)dr["PageID"], (int)dr["RoleID"], (string)dr["Text"]);
+                helpSystem.Add(hs);
+            }
+            conn.CloseConnection();
+            return helpSystem;
+        }
+    }
+}
---
+++ Workflow/Workflow/Models/HelpSystem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Workflow.Models
+{
+    public class HelpSystem
+    {
+        int pageID;
+        int roleID;
+        string text;
+
+        public HelpSystem(int pid, int rid, string text)
+        {
+            this.pageID = pid;
+            this.roleID = rid;
+            this.text = text;
+        }
+
+        public int PageID
+        {
+            get { return pageID; }
+            set { pageID = value; }
+        }
+
+        public int RoleID
+        {
+            get { return roleID; }
+            set { roleID = value; }
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+    }
+}
